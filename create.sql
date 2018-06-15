fe2826d61be2355f4b424a7548d9833d9a497ca9
Ruslan Yevdokymov <evruslan17@gmail.com>
2018-06-15 20:39:17 +0200

    Some changes to database structure

--- create.sql
+++ create.sql
@@ -12,6 +12,24 @@ DROP TABLE IF EXISTS lekarze_specjalizacje CASCADE;
 DROP TABLE IF EXISTS specjalizacje CASCADE;
 DROP TABLE IF EXISTS pracownicy CASCADE;
 DROP TABLE IF EXISTS pacjenci CASCADE;
+DROP VIEW IF EXISTS terminaz;
+DROP FUNCTION IF EXISTS wizyte_odbyte_pacjenta( INTEGER );
+DROP FUNCTION IF EXISTS wizyte_planowane_pacjenta( INTEGER );
+DROP FUNCTION IF EXISTS czy_aktywny_lekarz( INTEGER );
+DROP FUNCTION IF EXISTS lekarze_specjalizacje_check();
+DROP FUNCTION IF EXISTS pacjent_check();
+DROP FUNCTION IF EXISTS pesel_check( CHAR );
+DROP FUNCTION IF EXISTS pacjent_lpk_check();
+DROP FUNCTION IF EXISTS pracownik_check();
+DROP FUNCTION IF EXISTS ranking( DATE, DATE );
+DROP FUNCTION IF EXISTS ranking_alfabetyczny( DATE, DATE );
+DROP FUNCTION IF EXISTS ranking_cecha( DATE, DATE, VARCHAR );
+DROP FUNCTION IF EXISTS ranking_specjalizacje( DATE, DATE, INTEGER );
+DROP FUNCTION IF EXISTS role_pracownika( INTEGER );
+DROP FUNCTION IF EXISTS terminarz_insert();
+DROP FUNCTION IF EXISTS specjalizacje_lekarza( INTEGER );
+DROP FUNCTION IF EXISTS wizyta_odbyta_check();
+DROP FUNCTION IF EXISTS wizyta_planowana_check();
 
 CREATE TABLE pacjenci
 (
@@ -87,26 +105,26 @@ CREATE TABLE pracownicy_role
 
 CREATE TABLE cele_wizyty
 (
-  id_celu               SERIAL                  NOT NULL
+  id_celu SERIAL  NOT NULL
     PRIMARY KEY,
-  nazwa                 VARCHAR                 NOT NULL
-    UNIQUE,
-  specjalizacja_lekarza INTEGER
-    REFERENCES specjalizacje (id_specjalizacji) NOT NULL
+  nazwa   VARCHAR NOT NULL
+    UNIQUE
 );
 
 CREATE TABLE wizyty_odbyte
 (
-  id_wizyty    INTEGER   NOT NULL
+  id_wizyty     INTEGER   NOT NULL
     PRIMARY KEY,
-  id_pacjenta  INTEGER   NOT NULL
+  id_pacjenta   INTEGER   NOT NULL
     REFERENCES pacjenci (id_pacjenta),
-  id_lekarza   INTEGER   NOT NULL
+  id_lekarza    INTEGER   NOT NULL
     REFERENCES pracownicy (id_pracownika),
-  cel          INTEGER   NOT NULL
+  cel           INTEGER   NOT NULL
     REFERENCES cele_wizyty (id_celu),
-  data         TIMESTAMP NOT NULL,
-  czas_trwania INTERVAL  NOT NULL CHECK (czas_trwania > 0),
+  specjalizacja INTEGER   NOT NULL
+    REFERENCES specjalizacje (id_specjalizacji),
+  data          TIMESTAMP NOT NULL,
+  czas_trwania  INTERVAL  NOT NULL CHECK (czas_trwania > INTERVAL '0'),
   UNIQUE (id_pacjenta, id_lekarza, data)
 );
 
@@ -120,8 +138,10 @@ CREATE TABLE wizyty_planowane
     REFERENCES pracownicy (id_pracownika),
   cel            INTEGER   NOT NULL
     REFERENCES cele_wizyty (id_celu),
+  specjalizacja  INTEGER   NOT NULL
+    REFERENCES specjalizacje (id_specjalizacji),
   data           TIMESTAMP NOT NULL,
-  szacowany_czas INTERVAL CHECK (szacowany_czas > 0),
+  szacowany_czas INTERVAL CHECK (szacowany_czas > INTERVAL '0' ),
   UNIQUE (id_pacjenta, id_lekarza, data)
 );
 
@@ -200,7 +220,7 @@ language plpgsql;
 
 CREATE OR REPLACE FUNCTION wizyte_odbyte_pacjenta(id INTEGER)
   RETURNS TABLE(id_wizyty  INTEGER, id_pacjenta INTEGER,
-                id_lekarza INTEGER, cel INTEGER, data TIMESTAMP, czas_trwania INTERVAL)
+                id_lekarza INTEGER, cel INTEGER, specjalizacja INTEGER, data TIMESTAMP, czas_trwania INTERVAL)
 AS $$
 BEGIN
   RETURN QUERY
@@ -213,7 +233,7 @@ language plpgsql;
 
 CREATE OR REPLACE FUNCTION wizyte_planowane_pacjenta(id INTEGER)
   RETURNS TABLE(id_wizyty  INTEGER, id_pacjenta INTEGER,
-                id_lekarza INTEGER, cel INTEGER, data TIMESTAMP, szacowany_czas INTERVAL)
+                id_lekarza INTEGER, cel INTEGER, specjalizacja INTEGER, data TIMESTAMP, szacowany_czas INTERVAL)
 AS $$
 BEGIN
   RETURN QUERY
@@ -363,6 +383,7 @@ CREATE OR REPLACE VIEW terminaz AS
   SELECT
     id_pacjenta,
     cel,
+    specjalizacja,
     data
   FROM wizyty_planowane
   WHERE FALSE;
@@ -405,9 +426,7 @@ BEGIN
   i = make_interval(mins := (val :: INT));
   id = coalesce((SELECT ls.id_lekarza
                  FROM lekarze_specjalizacje ls
-                 WHERE ls.id_specjalizacji = (SELECT id_specjalizacji
-                                              FROM cele_wizyty
-                                              WHERE id_celu = NEW.cel)
+                 WHERE ls.id_specjalizacji = NEW.specjalizacja
                        AND (SELECT count(*)
                             FROM wizyty_planowane w
                             WHERE ls.id_lekarza = w.id_lekarza AND
