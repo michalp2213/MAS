6c96624064f8e4ad99b0db1107581012025c4266
Ruslan Yevdokymov <evruslan17@gmail.com>
2018-06-17 10:47:20 +0200

    Split create.sql into many files

---
+++ util/insertfunctions.sql
@@ -0,0 +1,459 @@
+CREATE OR REPLACE FUNCTION nulls(VARIADIC a ANYARRAY)
+  RETURNS INTEGER
+AS $$
+DECLARE amount NUMERIC;
+        i      INTEGER;
+BEGIN
+  amount = 0;
+  FOR i IN 1..array_length(a, 1) LOOP
+    IF a [i] IS NULL
+    THEN amount = amount + 1;
+    END IF;
+  END LOOP;
+  RETURN amount;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION role_pracownika(id INTEGER)
+  RETURNS TABLE(id_roli INTEGER, nazwa VARCHAR)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT r.*
+  FROM pracownicy_role p
+    JOIN role r ON p.id_roli = r.id_roli AND p.id_pracownika = id;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION specjalizacje_lekarza(id INTEGER)
+  RETURNS TABLE(id_specjalizazji INTEGER, nazwa VARCHAR)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT s.*
+  FROM lekarze_specjalizacje ls
+    JOIN specjalizacje s ON ls.id_specjalizacji = s.id_specjalizacji AND ls.id_lekarza = id;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION wizyte_odbyte_pacjenta(id INTEGER)
+  RETURNS TABLE(id_wizyty  INTEGER, id_pacjenta INTEGER,
+                id_lekarza INTEGER, cel INTEGER, specjalizacja INTEGER, data TIMESTAMP, czas_trwania INTERVAL)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT *
+  FROM wizyty_odbyte w
+  WHERE w.id_pacjenta = id;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION wizyte_planowane_pacjenta(id INTEGER)
+  RETURNS TABLE(id_wizyty  INTEGER, id_pacjenta INTEGER,
+                id_lekarza INTEGER, cel INTEGER, specjalizacja INTEGER, data TIMESTAMP, szacowany_czas INTERVAL)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT *
+  FROM wizyty_planowane w
+  WHERE w.id_pacjenta = id;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION czy_aktywny_lekarz(id INTEGER)
+  RETURNS BOOLEAN AS
+$$
+BEGIN
+  RETURN (SELECT count(*)
+          FROM pracownicy_role
+          WHERE id_pracownika = id AND id_roli = 1) = 1 AND (SELECT count(*)
+                                                             FROM pracownicy_role
+                                                             WHERE id_pracownika = id AND id_roli = 2) = 0;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION pesel_check(p CHAR(11))
+  RETURNS BOOLEAN AS
+$$
+DECLARE
+  pesel CHAR [];
+  s     INTEGER;
+BEGIN
+  pesel = string_to_array(p, NULL);
+  s =
+  (pesel [1] :: INT) + (pesel [2] :: INT) * 3 + (pesel [3] :: INT) * 7 + (pesel [4] :: INT) * 9 + pesel [5] :: INT * 1 +
+  pesel [6] :: INT * 3 + pesel [7] :: INT * 7 + pesel [8] :: INT * 9 + pesel [9] :: INT + pesel [10] :: INT * 3;
+  s = s % 10;
+  RETURN (10 - s) = pesel [11] :: INT;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION pacjent_check()
+  RETURNS TRIGGER AS $pacjent_check$
+BEGIN
+  IF NEW.pesel IS NULL OR length(NEW.pesel) <> 11
+  THEN RETURN NEW; END IF;
+  IF pesel_check(NEW.pesel)
+  THEN RETURN NEW;
+  ELSE RAISE EXCEPTION 'Niepoprawny PESEL';
+  END IF;
+END;
+$pacjent_check$
+language plpgsql;
+
+CREATE TRIGGER pacjent_check
+  BEFORE INSERT OR UPDATE
+  ON pacjenci
+  FOR EACH ROW EXECUTE PROCEDURE pacjent_check();
+
+CREATE OR REPLACE FUNCTION pracownik_check()
+  RETURNS TRIGGER AS $pracownik_check$
+BEGIN
+  IF pesel_check(NEW.pesel)
+  THEN RETURN NEW;
+  ELSE RAISE EXCEPTION 'Niepoprawny PESEL';
+  END IF;
+END;
+$pracownik_check$
+language plpgsql;
+
+CREATE TRIGGER pracownik_check
+  BEFORE INSERT OR UPDATE
+  ON pracownicy
+  FOR EACH ROW EXECUTE PROCEDURE pracownik_check();
+
+CREATE OR REPLACE FUNCTION pacjent_lpk_check()
+  RETURNS TRIGGER AS $pacjent_lpk_check$
+BEGIN
+  IF czy_aktywny_lekarz(NEW.id_lekarza)
+  THEN RETURN NEW;
+  ELSE RAISE EXCEPTION 'Nie aktywny lekarz';
+  END IF;
+END;
+$pacjent_lpk_check$
+language plpgsql;
+
+CREATE TRIGGER pacjent_lpk_check
+  BEFORE INSERT OR UPDATE
+  ON pacjenci_lpk
+  FOR EACH ROW EXECUTE PROCEDURE pacjent_lpk_check();
+
+CREATE OR REPLACE FUNCTION wizyta_odbyta_check()
+  RETURNS TRIGGER AS $wizyta_odbyta_check$
+DECLARE r RECORD;
+BEGIN
+  IF czy_aktywny_lekarz(NEW.id_lekarza) = FALSE
+  THEN RAISE EXCEPTION 'Nie jest lekarzem';
+  END IF;
+  FOR r in SELECT
+             data                AS d1,
+             data + czas_trwania AS d2
+           FROM wizyty_odbyte
+           WHERE id_lekarza = NEW.id_lekarza LOOP
+    IF (NEW.data, NEW.data + NEW.czas_trwania) OVERLAPS (r.d1, r.d2)
+    THEN RAISE EXCEPTION 'Lekarz jest zajenty';
+    END IF;
+  END LOOP;
+  RETURN NEW;
+END;
+$wizyta_odbyta_check$
+language plpgsql;
+
+CREATE TRIGGER wizyta_odbyta_check
+  BEFORE INSERT OR UPDATE
+  ON wizyty_odbyte
+  FOR EACH ROW EXECUTE PROCEDURE wizyta_odbyta_check();
+
+CREATE OR REPLACE FUNCTION wizyta_planowana_check()
+  RETURNS TRIGGER AS $wizyta_planowana_check$
+DECLARE r RECORD;
+BEGIN
+  IF NEW.data < current_time
+  THEN RAISE EXCEPTION 'Potrzebuje Panstwo DeLorean.';
+  END IF;
+  IF czy_aktywny_lekarz(NEW.id_lekarza) = FALSE
+  THEN RAISE EXCEPTION 'Nie jest lekarzem';
+  END IF;
+  FOR r in SELECT
+             data                  AS d1,
+             data + szacowany_czas AS d2
+           FROM wizyty_planowane
+           WHERE id_lekarza = NEW.id_lekarza LOOP
+    IF (NEW.data, NEW.data + NEW.szacowany_czas) OVERLAPS (r.d1, r.d2)
+    THEN RAISE EXCEPTION 'Lekarz jest zajenty';
+    END IF;
+  END LOOP;
+  RETURN NEW;
+END;
+$wizyta_planowana_check$
+language plpgsql;
+
+CREATE TRIGGER wizyta_planowana_check
+  BEFORE INSERT OR UPDATE
+  ON wizyty_planowane
+  FOR EACH ROW EXECUTE PROCEDURE wizyta_planowana_check();
+
+CREATE OR REPLACE VIEW terminaz AS
+  SELECT
+    id_pacjenta,
+    cel,
+    specjalizacja,
+    data
+  FROM wizyty_planowane
+  WHERE FALSE;
+
+CREATE OR REPLACE FUNCTION terminarz_insert()
+  RETURNS TRIGGER AS $terminarz_insert$
+DECLARE r   RECORD;
+        a   INTEGER;
+        val NUMERIC;
+        c   NUMERIC;
+        s   NUMERIC;
+        age INTEGER;
+        i   INTERVAL;
+        id  INTEGER;
+BEGIN
+  IF NEW.data < current_time
+  THEN RAISE EXCEPTION 'Potrzebuje Panstwo DeLorean';
+  END IF;
+  s = 0;
+  val = 0;
+  a = 40;
+  age = (SELECT extract(YEARS FROM (current_time - data_urodzenia))
+         FROM pacjenci
+         WHERE NEW.id_pacjenta = pacjenci.id_pacjenta);
+  FOR r IN SELECT
+             extract(MINUTES FROM w.czas_trwania)         AS t,
+             (SELECT extract(years from (current_time - data_urodzenia))
+              FROM pacjenci
+              WHERE w.id_pacjenta = pacjenci.id_pacjenta) AS age
+           FROM wizyty_odbyte w
+           WHERE w.cel = NEW.cel LOOP
+    c = exp(-(((@(r.age - age)) ^ 2) / (2 * (a ^ 2))));
+    val = val + r.t * c;
+    s = s + c;
+  END LOOP;
+  IF s = 0
+  THEN val = 30;
+  ELSE val = val / s;
+  END IF;
+  i = make_interval(mins := (val :: INT));
+  id = coalesce((SELECT ls.id_lekarza
+                 FROM lekarze_specjalizacje ls
+                 WHERE ls.id_specjalizacji = NEW.specjalizacja
+                       AND (SELECT count(*)
+                            FROM wizyty_planowane w
+                            WHERE ls.id_lekarza = w.id_lekarza AND
+                                  (w.data, w.data + w.szacowany_czas)
+                                  OVERLAPS
+                                  (New.data, New.data + i)) = 0
+                 ORDER BY id_lekarza
+                 LIMIT 1), -1);
+  IF id = -1
+  THEN RAISE EXCEPTION 'Nie mamy specjalisty';
+  END IF;
+  INSERT INTO wizyty_planowane (id_pacjenta, id_lekarza, cel, data, szacowany_czas)
+  VALUES (NEW.id_pacjenta, id, NEW.cel, NEW.data, i);
+  RETURN NEW;
+END;
+$terminarz_insert$
+language plpgsql;
+
+CREATE TRIGGER terminarz_insert
+  INSTEAD OF INSERT
+  ON terminaz
+  FOR EACH ROW EXECUTE PROCEDURE terminarz_insert();
+
+CREATE OR REPLACE FUNCTION ranking(d1 DATE, d2 DATE)
+  RETURNS TABLE(id_lekarza INTEGER, imie VARCHAR, nazwisko VARCHAR, wynik NUMERIC(3, 2))
+AS
+$$
+BEGIN
+  d1 = coalesce(d1, min((SELECT data
+                         FROM ankiety_lekarze)));
+  d2 = coalesce(d2, max((SELECT data
+                         FROM ankiety_lekarze)));
+  RETURN QUERY
+  WITH temp AS (
+      SELECT
+        al.id_lekarza                                                               AS id,
+        sum(coalesce(al.dokladnosc_badan, 0)) / CASE WHEN count(al.dokladnosc_badan) = 0
+          THEN 1
+                                                ELSE count(al.dokladnosc_badan) END AS v1,
+        sum(coalesce(al.informacyjnosc, 0)) / CASE WHEN count(al.informacyjnosc) = 0
+          THEN 1
+                                              ELSE count(al.informacyjnosc) END     AS v2,
+        sum(coalesce(al.opanowanie, 0)) / CASE WHEN count(al.opanowanie) = 0
+          THEN 1
+                                          ELSE count(al.opanowanie) END             AS v3,
+        sum(coalesce(al.uprzejmosc, 0)) / CASE WHEN count(al.uprzejmosc) = 0
+          THEN 1
+                                          ELSE count(al.uprzejmosc) END             AS v4
+      FROM ankiety_lekarze al
+      WHERE data >= d1 AND data <= d2
+      GROUP BY al.id_lekarza
+  )
+  SELECT
+    id,
+    p.imie,
+    p.nazwisko,
+    ((v1 + v2 + v3 + v4) /
+     (nulls(nullif(v1, 0)) + nulls(nullif(v2, 0)) + nulls(nullif(v3, 0)) + nulls(nullif(v4, 0)))) :: NUMERIC(3, 2)
+  FROM temp
+    JOIN pracownicy p ON id = p.id_pracownika
+  ORDER BY (v1 + v2 + v3 + v4) /
+           (nulls(nullif(v1, 0)) + nulls(nullif(v2, 0)) + nulls(nullif(v3, 0)) + nulls(nullif(v4, 0))) DESC, id;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION lekarze_specjalizacje_check()
+  RETURNS TRIGGER AS $lekarze_specjalizacje_check$
+BEGIN
+  IF czy_aktywny_lekarz(NEW.id_lekarza)
+  THEN RETURN NEW;
+  ELSE RAISE EXCEPTION 'Nie jest lekarzem';
+  END IF;
+END;
+$lekarze_specjalizacje_check$
+language plpgsql;
+
+CREATE TRIGGER lekarze_specjalizacje_check
+  BEFORE INSERT OR UPDATE
+  ON lekarze_specjalizacje
+  FOR EACH ROW EXECUTE PROCEDURE lekarze_specjalizacje_check();
+
+CREATE OR REPLACE FUNCTION ranking_cecha(d1 DATE, d2 DATE, cecha VARCHAR)
+  RETURNS TABLE(id_lekarza INTEGER, imie VARCHAR, nazwisko VARCHAR, wynik NUMERIC(3, 2))
+AS
+$$
+BEGIN
+  d1 = coalesce(d1, min((SELECT data
+                         FROM ankiety_lekarze)));
+  d2 = coalesce(d2, max((SELECT data
+                         FROM ankiety_lekarze)));
+  IF cecha <> 'dokladnosc_badan' AND cecha <> 'informacyjnosc' AND cecha <> 'opanowanie' AND cecha <> 'uprzejmosc'
+  THEN RAISE EXCEPTION 'Nie mamy takiej cechy';
+  END IF;
+  IF cecha = 'dokladnosc_badan'
+  THEN
+    RETURN QUERY
+    WITH temp AS (
+        SELECT
+          al.id_lekarza                                                                         AS id,
+          (sum(coalesce(al.dokladnosc_badan, 0)) / count(al.dokladnosc_badan)) :: NUMERIC(3, 2) AS v
+        FROM ankiety_lekarze al
+        WHERE data >= d1 AND data <= d2
+        GROUP BY al.id_lekarza
+        HAVING count(al.dokladnosc_badan) > 0
+    )
+    SELECT
+      p.id_pracownika,
+      p.imie,
+      p.nazwisko,
+      v
+    FROM temp
+      JOIN pracownicy p ON id = p.id_pracownika
+    ORDER BY v DESC, id;
+  ELSEIF cecha = 'informacyjnosc'
+    THEN
+      RETURN QUERY
+      WITH temp AS (
+          SELECT
+            al.id_lekarza                                                                     AS id,
+            (sum(coalesce(al.informacyjnosc, 0)) / count(al.informacyjnosc)) :: NUMERIC(3, 2) AS v
+          FROM ankiety_lekarze al
+          WHERE data >= d1 AND data <= d2
+          GROUP BY al.id_lekarza
+          HAVING count(al.informacyjnosc) > 0
+      )
+      SELECT
+        p.id_pracownika,
+        p.imie,
+        p.nazwisko,
+        v
+      FROM temp
+        JOIN pracownicy p ON id = p.id_pracownika
+      ORDER BY v DESC, id;
+  ELSEIF cecha = 'opanowanie'
+    THEN
+      RETURN QUERY
+      WITH temp AS (
+          SELECT
+            al.id_lekarza                                                             AS id,
+            (sum(coalesce(al.opanowanie, 0)) / count(al.opanowanie)) :: NUMERIC(3, 2) AS v
+          FROM ankiety_lekarze al
+          WHERE data >= d1 AND data <= d2
+          GROUP BY al.id_lekarza
+          HAVING count(al.opanowanie) > 0
+      )
+      SELECT
+        p.id_pracownika,
+        p.imie,
+        p.nazwisko,
+        v
+      FROM temp
+        JOIN pracownicy p ON id = p.id_pracownika
+      ORDER BY v DESC, id;
+  ELSE
+    RETURN QUERY
+    WITH temp AS (
+        SELECT
+          al.id_lekarza                                                             AS id,
+          (sum(coalesce(al.uprzejmosc, 0)) / count(al.uprzejmosc)) :: NUMERIC(3, 2) AS v
+        FROM ankiety_lekarze al
+        WHERE data >= d1 AND data <= d2
+        GROUP BY al.id_lekarza
+        HAVING count(al.uprzejmosc) > 0
+    )
+    SELECT
+      p.id_pracownika,
+      p.imie,
+      p.nazwisko,
+      v
+    FROM temp
+      JOIN pracownicy p ON id = p.id_pracownika
+    ORDER BY v DESC, id;
+  END IF;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION ranking_specjalizacje(d1 DATE, d2 DATE, id INTEGER)
+  RETURNS TABLE(id_lekarza INTEGER, imie VARCHAR, nazwisko VARCHAR, wynik NUMERIC(3, 2))
+AS
+$$
+BEGIN
+  d1 = coalesce(d1, min((SELECT data
+                         FROM ankiety_lekarze)));
+  d2 = coalesce(d2, max((SELECT data
+                         FROM ankiety_lekarze)));
+  RETURN QUERY SELECT r.*
+               FROM ranking(d1, d2) r
+                 JOIN lekarze_specjalizacje ls ON ls.id_lekarza = r.id_lekarza AND ls.id_specjalizacji = id;
+END;
+$$
+language plpgsql;
+
+CREATE OR REPLACE FUNCTION ranking_alfabetyczny(d1 DATE, d2 DATE)
+  RETURNS TABLE(id_lekarza INTEGER, imie VARCHAR, nazwisko VARCHAR, wynik NUMERIC(3, 2))
+AS
+$$
+BEGIN
+  d1 = coalesce(d1, min((SELECT data
+                         FROM ankiety_lekarze)));
+  d2 = coalesce(d2, max((SELECT data
+                         FROM ankiety_lekarze)));
+  RETURN QUERY SELECT r.*
+               FROM ranking(d1, d2) r
+               ORDER BY r.nazwisko, r.imie, r.wynik DESC;
+END;
+$$
+language plpgsql;
---
+++ util/insertpacj.sql
@@ -0,0 +1,160 @@
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Józef', 'Wasilewski', '67102435341', '24.10.1967', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Mateusz', 'Wójcik', '59091439262', '14.09.1959', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Waldemar', 'Wysocki', '02312279846', '22.11.2002', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Janusz', 'Kozłowski', '60051035989', '10.05.1960', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Tadeusz', 'Wojciechowski', '88042172628', '21.04.1988', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Wojciech', 'Górski', '63120726568', '07.12.1963', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Grzegorz', 'Bąk', '82013145947', '31.01.1982', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Władysław', 'Brzeziński', '71091543282', '15.09.1971', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Robert', 'Szewczyk', '61032585921', '25.03.1961', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jacek', 'Sikorski', '83081848167', '18.08.1983', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Marcin', 'Jakubowski', '93100778683', '07.10.1993', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Janusz', 'Szymański', '64101132385', '11.10.1964', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Kazimierz', 'Rutkowski', '97111842447', '18.11.1997', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Tomasz', 'Krawczyk', '72112067189', '20.11.1972', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jan', 'Jankowski', '54111394541', '13.11.1954', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Ryszard', 'Wiśniewski', '58050737223', '07.05.1958', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Łukasz', 'Borkowski', '94070365284', '03.07.1994', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Mariusz', 'Witkowski', '45071191965', '11.07.1945', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Michał', 'Stępień', '79081091469', '10.08.1979', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Michał', 'Rutkowski', '57073131241', '31.07.1957', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Piotr', 'Baranowski', '64111922549', '19.11.1964', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Waldemar', 'Kucharski', '65121145243', '11.12.1965', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Rafał', 'Brzeziński', '91100759288', '07.10.1991', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Józef', 'Kwiatkowski', '50030653145', '06.03.1950', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Sebastian', 'Wasilewski', '49110391446', '03.11.1949', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Grzegorz', 'Borkowski', '90080555547', '05.08.1990', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jarosław', 'Chmielewski', '58072845467', '28.07.1958', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Przemysław', 'Witkowski', '65032579265', '25.03.1965', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Zdzisław', 'Kowalski', '56051759721', '17.05.1956', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jarosław', 'Zając', '80070626324', '06.07.1980', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jakub', 'Baranowski', '93120789665', '07.12.1993', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Wiesław', 'Wilk', '63012813747', '28.01.1963', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Artur', 'Adamski', '67032997483', '29.03.1967', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Marian', 'Wieczorek', '44091647447', '16.09.1944', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Kamil', 'Wiśniewski', '74102872445', '28.10.1974', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Daniel', 'Zakrzewski', '75101422167', '14.10.1975', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Roman', 'Chmielewski', '52052714268', '27.05.1952', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Marcin', 'Ostrowski', '55031924845', '19.03.1955', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jacek', 'Czarnecki', '78092095862', '20.09.1978', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Grzegorz', 'Kozłowski', '87111153764', '11.11.1987', 'M');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Monika', 'Zakrzewska', '73110429148', '04.11.1973', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Urszula', 'Kucharska', '57062384649', '23.06.1957', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Patrycja', 'Baranowska', '74031472882', '14.03.1974', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Krystyna', 'Czerwińska', '64071584865', '15.07.1964', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Janina', 'Stępień', '02280826686', '08.08.2002', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Marzena', 'Gajewska', '44080853181', '08.08.1944', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Stefania', 'Nowakowska', '75040438984', '04.04.1975', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Stefania', 'Jasińska', '68081285925', '12.08.1968', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Aleksandra', 'Wojciechowska', '51122641628', '26.12.1951', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Danuta', 'Szewczyk', '75071711588', '17.07.1975', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Danuta', 'Mazur', '56082443983', '24.08.1956', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Magdalena', 'Ziółkowska', '60051975885', '19.05.1960', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Marzena', 'Rutkowska', '50110998663', '09.11.1950', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Natalia', 'Nowicka', '89011363366', '13.01.1989', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Dorota', 'Wieczorek', '63040163625', '01.04.1963', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Aneta', 'Głowacka', '77012219441', '22.01.1977', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Grażyna', 'Malinowska', '93122243424', '22.12.1993', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Grażyna', 'Sokołowska', '00272628786', '26.07.2000', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Teresa', 'Czarnecka', '60041452422', '14.04.1960', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Halina', 'Jankowska', '95112456168', '24.11.1995', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Jolanta', 'Kowalska', '84100214424', '02.10.1984', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Ewelina', 'Michalska', '68081254727', '12.08.1968', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Irena', 'Szulc', '62050988183', '09.05.1962', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Bożena', 'Malinowska', '59041562549', '15.04.1959', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Danuta', 'Zalewska', '53081336146', '13.08.1953', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Stefania', 'Borkowska', '43020735262', '07.02.1943', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Teresa', 'Duda', '43082686586', '26.08.1943', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Justyna', 'Wiśniewska', '87112327267', '23.11.1987', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Agata', 'Lis', '65031015368', '10.03.1965', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Elżbieta', 'Sikorska', '62022645687', '26.02.1962', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Agnieszka', 'Kowalczyk', '84102329267', '23.10.1984', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Natalia', 'Szewczyk', '49052319982', '23.05.1949', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Dorota', 'Gajewska', '90022063644', '20.02.1990', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Agata', 'Szczepańska', '90112128882', '21.11.1990', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Patrycja', 'Ziółkowska', '57112476285', '24.11.1957', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Bożena', 'Sikorska', '81053097227', '30.05.1981', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Danuta', 'Kaźmierczak', '54122515924', '25.12.1954', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Danuta', 'Kaczmarek', '78031127261', '11.03.1978', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Aleksandra', 'Tomaszewska', '97122423246', '24.12.1997', 'F');
+INSERT INTO pacjenci (imie, nazwisko, pesel, data_urodzenia, plec)
+VALUES ('Alicja', 'Kucharska', '49010814683', '08.01.1949', 'F');
