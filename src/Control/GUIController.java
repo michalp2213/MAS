928d538a30c7f445e535919fa23f7e6c5d4d0303
psimaj <przemyslaw.simajchel@gmail.com>
2018-06-22 02:23:26 +0200

    LPK

--- src/Control/GUIController.java
+++ src/Control/GUIController.java
@@ -876,7 +876,7 @@ public class GUIController {
         updateComboBox(LPKLekarzeComboBox, getSmallLekarze());
         updateComboBox(LPKPacjenciComboBox, getSmallPacjenci());
         try {
-            ArrayList<ArrayList<String>> kozak = Database.executeQuery("select lrul();");
+            ArrayList<ArrayList<String>> kozak = Database.executeQuery("select * from lrul();");
             String sklejonyKozak = kozak.get(1).get(0) + ", " + kozak.get(1).get(1) + ", " + kozak.get(1).get(2);
             LPKLekarzeComboBox.getSelectionModel().select(sklejonyKozak);
         } catch (SQLException e) {
